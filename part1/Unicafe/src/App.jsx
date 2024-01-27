6ac29ac4374509ead0f41b4f9bf6f13ad73bfde5
rakshithapatel08 <rakshithapatel0807@gmail.com>
2024-01-27 13:40:41 +0530

    exercise 1.8

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -1,45 +1,53 @@
 import { useState } from "react"
 
-function App() { 
-  const [good,setGood] = useState(0);
-  const [neutral,setNeutral] = useState(0);
-  const [bad,setBad] = useState(0);
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const handleGood = ()=>{
-    setGood(good+1);
+  const handleGood = () => {
+    setGood(good + 1);
   }
 
-  const handleNeutral = ()=>{
-    setNeutral(neutral+1);
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
   }
 
-  const handleBad = ()=>{
-    setBad(bad+1);
+  const handleBad = () => {
+    setBad(bad + 1);
   }
 
   return (
     <>
-     <h1>Give your feedback</h1>
-     <Button onClick={handleGood} text="good"/>
-     <Button onClick={handleNeutral} text="neutral"/>
-     <Button onClick={handleBad} text="bad"/>
-
-     <h1>Statistics</h1>
-     <p>good = {good}</p>
-     <p>neutral = {neutral}</p>
-     <p>bad = {bad}</p>
-     <p>total = {good + neutral + bad}</p>
-     <p>average = {(good-bad)/(good + neutral + bad)}</p>
-     <p>positive percentage = {good/(good + neutral + bad) *100} %</p>
+      <h1>Give your feedback</h1>
+      <Button onClick={handleGood} text="good" />
+      <Button onClick={handleNeutral} text="neutral" />
+      <Button onClick={handleBad} text="bad" />
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </>
   )
 }
 
 // refactoring to a button component
-const Button = (props)=>{
-  return(
+const Button = (props) => {
+  return (
     <>
-    <button onClick={props.onClick}>{props.text}</button>
+      <button onClick={props.onClick}>{props.text}</button>
+    </>
+  )
+}
+
+const Statistics = ({ good, bad, neutral }) => {
+  // destructured the props object 
+  return (
+    <>
+      <h1>Statistics</h1>
+      <p>good = {good}</p>
+      <p>neutral = {neutral}</p>
+      <p>bad = {bad}</p>
+      <p>total = {good + neutral + bad}</p>
+      <p>average = {(good - bad) / (good + neutral + bad)}</p>
+      <p>positive percentage = {good / (good + neutral + bad) * 100} %</p>
     </>
   )
 }
