0971a9cfdf6b70ba8f79d3307b113983f66bb937
rakshithapatel08 <rakshithapatel0807@gmail.com>
2024-02-05 11:07:27 +0530

    exercise 2.3

--- part2/courseInfo/src/components/Total.jsx
+++ part2/courseInfo/src/components/Total.jsx
@@ -1,11 +1,14 @@
 const Total = ({total})=>{
-    let sum=0;
+    // let sum=0;
     const exArray = total.map((part=>part.exercises))
-    for(let i=0;i<exArray.length;i++){
-        sum=sum+exArray[i];
-    }
+    console.log(exArray);
+    // for(let i=0;i<exArray.length;i++){
+    //     sum=sum+exArray[i];
+    // }
+    const sum = exArray.reduce((sum,current)=>sum+current,0)
     return(
         <>
+        {}
         <h3>total number of exercises are {sum}</h3>
         </>
     )
