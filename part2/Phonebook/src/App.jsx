c9a295ea1d40bcd2bc0523e62c03fab25280ef47
rakshithapatel08 <rakshithapatel0807@gmail.com>
2024-02-11 11:56:23 +0530

    exercise 2.9

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react"
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas",number:"040-1234567" }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newname, setNewname] = useState("")
   const [newnumber,setNewnumber] = useState("")
+  const [filter, setFilter] = useState("")
+  
+  const filteredArray = persons.filter((person)=>person.name.toLowerCase().includes(filter.toLowerCase()))
 
   const addName = (event) => {
     event.preventDefault();
@@ -19,7 +27,7 @@ const App = () => {
 
     });
     if (flag === 0) {
-      setPersons(persons.concat({ name: newname,number: newnumber }));
+      setPersons(persons.concat({ name: newname, number: newnumber }));
       setNewname("")
       setNewnumber("")
     }
@@ -28,6 +36,9 @@ const App = () => {
   return (
     <>
       <h1>Phonebook</h1>
+      <p>filter using names : search here</p>
+      <input type="text" placeholder="search by name" value={filter} onChange={(e)=>setFilter(e.target.value)}/>
+      <h2>Add new numbers</h2>
       <form onSubmit={addName}>
         name : <input type="text" placeholder="add new name" value={newname} onChange={(e) => setNewname(e.target.value)} />
         <br/>
@@ -35,10 +46,10 @@ const App = () => {
         <br/>
         <button type="submit">Add</button>
       </form>
-      <h1>Numbers</h1>
-      {persons.map((person, i) => {
+      <h2>Numbers</h2>     
+       {filteredArray.map((person) => {
         return (
-          <div key={i}>
+          <div key={person.id}>
             {person.name} {person.number}
             <br />
           </div>
