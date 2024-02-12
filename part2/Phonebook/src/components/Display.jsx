63c24f785355975c8ea8880aab719d60f3a81a4c
rakshithapatel08 <rakshithapatel0807@gmail.com>
2024-02-12 20:19:45 +0530

    exercise 2.14

--- part2/Phonebook/src/components/Display.jsx
+++ part2/Phonebook/src/components/Display.jsx
@@ -1,10 +1,20 @@
+import axios from "axios"
 const Display = ({filteredArray}) => {
+
+  const removePerson = (id,name)=>{
+    console.log(id)
+    const confirmation = window.confirm(`delete ${name}?`)
+    if(confirmation){
+      axios.delete(`http://localhost:3001/persons/${id}`)
+      .then(res=>console.log(`${res.data.name} was deleted`))
+    }    
+  }
   return (
     <>
       {filteredArray.map((person) => {
         return (
           <div key={person.id}>
-            {person.name} {person.number}
+            {person.name} {person.number} <button onClick={()=>removePerson(person.id,person.name)}>delete</button>
             <br />
           </div>
         )
