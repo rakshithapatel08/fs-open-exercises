d17d6d68f4337580ed21b629244f53a8ad056421
rakshithapatel08 <rakshithapatel0807@gmail.com>
2024-02-17 11:01:26 +0530

    exercise 2.20

--- part2/CountriesData/src/components/Countries.jsx
+++ part2/CountriesData/src/components/Countries.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+const api_key = import.meta.env.VITE_API_KEY
+
 const Countries = ({ filteredArray }) => {
+    const [weather,setWeather] = useState([])
     // console.log(filteredArray[0])
     const languages = filteredArray.map((item) => item.languages)
-    console.log(languages)
+    // console.log(languages)
 
     const values = Object.values(languages[0])
-    console.log(values)
+    // console.log(values)
+
+    // console.log(weather)
+
+    useEffect(()=>{
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${filteredArray[0].capital[0]}&appid=${api_key}`)
+        .then(res=>setWeather(res.data))
+    },[filteredArray])
 
     return (
         <>
@@ -17,6 +30,10 @@ const Countries = ({ filteredArray }) => {
                 {values.map((value) => <li key={value}>{value}</li>)}
             </ul>
             <img src={filteredArray[0].flags.png} alt="flagImage" />
+            <h3>Weather in {filteredArray[0].capital[0]}</h3>
+            <p>temperature is {weather.main?.temp}</p>
+            <p>wind speed is {weather.wind?.speed}</p>
+            <img src={`https://openweathermap.org/img/wn/${weather.weather?.[0].icon}.png`} width="100px" />            
         </>
     )
 }
