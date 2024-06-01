a4c3c5220ba7799c49585bf791b0e7729ce0f1ce
rakshithapatel08 <rakshithapatel0807@gmail.com>
2024-06-01 23:55:57 +0530

    exercise 4.7

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -48,10 +48,11 @@ const mostBlogs = (blogs)=>{
     for (const bloggs in countBloggers) {
        countBlogsArray.push({author:bloggs,blogs:countBloggers[bloggs]})
     }
-    const max = 0;
+    let max = 0;
     let maxObj = {}
     countBlogsArray.forEach((b)=>{
         if(b.blogs>max){
+            max = b.blogs
             maxObj={
                 author:b.author,
                 blogs:b.blogs
@@ -61,6 +62,38 @@ const mostBlogs = (blogs)=>{
 return maxObj
 }
 
+const mostLikes = (blogs)=>{
+    if(blogs.length === 0){
+        return {}
+    }
+    const likesByAuthor = blogs.reduce((acc, blog) => {
+        if (acc[blog.author]) {
+          acc[blog.author] += blog.likes;
+        } else {
+          acc[blog.author] = blog.likes;
+        }
+        return acc;
+      }, {})
+      let likesBlogsArray = [];
+      for (const bloggs in likesByAuthor) {
+        likesBlogsArray.push({author:bloggs,likes:likesByAuthor[bloggs]})
+     }
+
+      let max = 0;
+      let maxObj = {}
+      likesBlogsArray.forEach((b)=>{
+          if(b.likes>max){
+            max=b.likes
+              maxObj={
+                  author:b.author,
+                  likes:b.likes
+              }
+          }
+      })
+      console.log(maxObj)
+  return maxObj    
+}
+
 module.exports = {
-    dummy, totalLikes, favouriteBlog, mostBlogs
+    dummy, totalLikes, favouriteBlog, mostBlogs, mostLikes
 }
